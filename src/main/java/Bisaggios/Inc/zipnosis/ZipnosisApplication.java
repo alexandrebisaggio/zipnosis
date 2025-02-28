4b554add4eebba3749dbd144608e7b71502c1082
bisaggio <alexandrebisaggio@users.noreply.github.com>
2025-02-27 21:50:29 -0300

    Initial Commit

---
+++ src/main/java/Bisaggios/Inc/zipnosis/ZipnosisApplication.java
@@ -0,0 +1,13 @@
+package Bisaggios.Inc.zipnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipnosisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipnosisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Bisaggios/Inc/zipnosis/ZipnosisApplicationTests.java
@@ -0,0 +1,13 @@
+package Bisaggios.Inc.zipnosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipnosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
